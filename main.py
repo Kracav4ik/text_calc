600774651e56d5284f839e5adbdfac814a86570a
Kracav4ik <dikama2013@yandex.ru>
2016-04-30 02:27:03 +0300

    int() вручную)

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 
+NAN = '<not a number>'
+
+DIGITS = {'0123456789'[i]: i for i in range(10)}
+
+
+def to_number(s):
+    if not s:
+        return NAN
+    result = 0
+    for char in s:
+        if char not in DIGITS:
+            return NAN
+        result = result*10 + DIGITS[char]
+    return result
+
 
 if __name__ == '__main__':
     print('Calculator')
@@ -6,5 +21,6 @@ if __name__ == '__main__':
         line = input('>>> ')
         if not line:
             break
-        print('line is "%s"' % line)
+        number = to_number(line)
+        print('line is "%s" of type %s' % (number, type(number)))
     print('done!')
