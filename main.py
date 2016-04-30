9cef0372f45d6a292c19a4cb026aba4a9b818edc
Kracav4ik <dikama2013@yandex.ru>
2016-04-30 03:05:05 +0300

    AST для чисел

--- main.py
+++ main.py
@@ -4,6 +4,22 @@ NAN = '<not a number>'
 DIGITS = {'0123456789'[i]: i for i in range(10)}
 
 
+class ASTNode:
+    def calc(self):
+        pass
+
+    def __str__(self):
+        return str(self.calc())
+
+
+class ASTNumber(ASTNode):
+    def __init__(self, number):
+        self.number = number
+
+    def calc(self):
+        return self.number
+
+
 def to_number(s):
     if not s:
         return NAN
@@ -12,7 +28,7 @@ def to_number(s):
         if char not in DIGITS:
             return NAN
         result = result*10 + DIGITS[char]
-    return result
+    return ASTNumber(result)
 
 
 if __name__ == '__main__':
